f24240c95b5fe07fc6ebc2c025d9536006037181
user.email <pasquelin@Alban.local>
2026-07-29 18:46:04 +0200

    chore: eslint ignore dist artifacts

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(...tseslint.configs.recommended, {
+export default tseslint.config({ ignores: ['**/dist/**', '**/node_modules/**'] }, ...tseslint.configs.recommended, {
   rules: {
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
